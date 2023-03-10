01dea519e97587aef0951cfa4262001163efbd8a
irigovasker <irigovasker@yandex.ru>
2023-03-10 20:06:14 +0300

    feat: fix tests

--- src/test/java/ru/yandex/practicum/filmorate/util/dao/StoragesTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/util/dao/StoragesTests.java
@@ -11,6 +11,7 @@ import ru.yandex.practicum.filmorate.models.Genre;
 import ru.yandex.practicum.filmorate.models.Rating;
 import ru.yandex.practicum.filmorate.models.User;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.user.Relation;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.time.LocalDate;
@@ -22,14 +23,14 @@ import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
-public class FilmStorageTest {
+public class StoragesTests {
     private final FilmStorage filmStorage;
     private final UserStorage userStorage;
     static List<Film> filmList;
     static List<User> users;
 
     @Autowired
-    public FilmStorageTest(@Qualifier("filmDAO") FilmStorage filmStorage, @Qualifier("userDAO") UserStorage userStorage) {
+    public StoragesTests(@Qualifier("filmDAO") FilmStorage filmStorage, @Qualifier("userDAO") UserStorage userStorage) {
         this.filmStorage = filmStorage;
         this.userStorage = userStorage;
     }
@@ -57,24 +58,35 @@ public class FilmStorageTest {
 
         users = new ArrayList<>();
         User user = new User();
-        user.setName("Test");
-        user.setLogin("logTest");
-        user.setEmail("test@test.ru");
+        user.setName("testName");
+        user.setLogin("testLogin");
+        user.setEmail("test@Email.ru");
         user.setBirthday(LocalDate.of(2000, 1, 1));
         users.add(user);
 
         user = new User();
-        user.setName("test1");
-        user.setLogin("logTest1");
-        user.setEmail("test1@test.ru");
+        user.setName("testName1");
+        user.setLogin("testLogin1");
+        user.setLogin("testLogin1");
+        user.setEmail("test1@Email.ru");
         user.setBirthday(LocalDate.of(2000, 1, 2));
         users.add(user);
     }
 
     @Test
     void implementedMethodTest() {
-        userStorage.createUser(users.get(0));
-        userStorage.createUser(users.get(1));
+        //createUser() and getUserById() and getAll()
+        User userWithId1 = users.get(0);
+        User userWithId2 = users.get(1);
+        userStorage.createUser(userWithId1);
+        userStorage.createUser(userWithId2);
+
+        User userFromDB = userStorage.getUserById(1).orElse(new User());
+        User userFromDB2 = userStorage.getUserById(2).orElse(new User());
+        assertEquals(userWithId1, userFromDB);
+        assertEquals(userWithId2, userFromDB2);
+        assertIterableEquals(users, userStorage.getAll());
+
         //createFilm() and getFilmById()
         Film filmWithId1 = filmList.get(0);
         filmStorage.createFilm(filmList.get(0));
@@ -117,6 +129,33 @@ public class FilmStorageTest {
         filmStorage.removeFilm(2);
         List<Film> films = filmStorage.getAll();
         assertEquals(0, films.size());
-    }
 
+        //updateUser()
+        userWithId1.setName("updateName");
+        userWithId1.setEmail("update@email.com");
+        userStorage.updateUser(userWithId1);
+        userFromDB = userStorage.getUserById(1).orElse(new User());
+        assertEquals("updateName", userFromDB.getName());
+        assertEquals("update@email.com", userFromDB.getEmail());
+
+        //addRelation() and getUserFriends() and getSubscribers()
+        userStorage.addRelation(1, 2);
+        assertEquals(2, userStorage.getUserFriends(1).get(0).getId());
+        assertEquals(1, userStorage.getSubscribers(2).get(0).getId());
+
+        //changeRelationStatus() and getRelation()
+        Relation relation = userStorage.getRelation(1, 2);
+        userStorage.changeRelationStatus(relation, 3);
+        assertEquals(1, userStorage.getUserFriends(2).get(0).getId());
+
+        //removeRelation()
+        userStorage.removeRelation(relation);
+        assertEquals(0, userStorage.getUserFriends(1).size());
+        assertEquals(0, userStorage.getUserFriends(2).size());
+
+        //removeUser()
+        userStorage.removeUser(1);
+        userStorage.removeUser(2);
+        assertEquals(0, userStorage.getAll().size());
+    }
 }
