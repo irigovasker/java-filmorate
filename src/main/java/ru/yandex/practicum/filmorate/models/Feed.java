affd36a2a58d9cd9669c670d319461d01901b205
RybkinMike <Misha.tes-03@mail.ru>
2023-04-02 19:12:12 +0300

    refactor: добавлен функционал по заданию add-feed. рев2

--- src/main/java/ru/yandex/practicum/filmorate/models/Feed.java
+++ src/main/java/ru/yandex/practicum/filmorate/models/Feed.java
@@ -1,2 +1,15 @@
-package ru.yandex.practicum.filmorate.models;public class Feed {
+package ru.yandex.practicum.filmorate.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Feed {
+    private Integer eventId;
+    private Long timestamp;
+    private Integer userId;
+    private String eventType;
+    private String operation;
+    private Integer entityId;
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/FeedDAO.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FeedDAO.java
@@ -1,2 +1,38 @@
-package ru.yandex.practicum.filmorate.storage;public class FeedDAO {
+package ru.yandex.practicum.filmorate.storage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.models.Feed;
+import javax.sql.DataSource;
+import java.util.List;
+
+@Component
+public class FeedDAO {
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
+
+    @Autowired
+    public FeedDAO(JdbcTemplate jdbcTemplate, DataSource dataSource) {
+        this.jdbcTemplate = jdbcTemplate;
+        simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("\"feed\"")
+                .usingGeneratedKeyColumns("event_id")
+                .usingColumns("timestamp", "user_id", "event_type", "operation", "entity_id");
+    }
+
+    public void writeFeed (int userId, String eventType, String operation, Integer entityId) {
+        jdbcTemplate.update("INSERT INTO \"feed\" (timestamp, user_id, event_type, operation, entity_id) VALUES ( ?, ?, ?, ?, ? )",
+                System.currentTimeMillis(), userId, eventType, operation, entityId);
+    }
+
+    public List<Feed> getUserFeed(int userId) {
+        return jdbcTemplate.query(
+                        "SELECT * FROM \"feed\" AS f " +
+                        "WHERE f.USER_ID = ?",
+                new BeanPropertyRowMapper<>(Feed.class), userId
+        );
+    }
 }
