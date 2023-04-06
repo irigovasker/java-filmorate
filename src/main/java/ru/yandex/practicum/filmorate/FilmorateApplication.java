eb808f28d9d0d1866763ac742fcb1fc8cd77139c
irigovasker <113387741+irigovasker@users.noreply.github.com>
2023-04-06 23:23:37 +0300

    Update FilmorateApplication.java

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -1,5 +1,5 @@
 package ru.yandex.practicum.filmorate;
-//Привет. Реализовал тз
+//Привет. На счет Relation я реализовал так еще до групового проекта что бы была возможность иметь 3 статуса дружбы. DataAccess это исключение спринга поэтому его нет в проекте
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
